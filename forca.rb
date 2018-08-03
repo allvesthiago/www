75a2e43359fb55c98344b71c17801c94b60a190b
Thiago <31213162+allvesthiago@users.noreply.github.com>
2018-08-03 19:18:14 -0300

    3.5

--- forca.rb
+++ forca.rb
@@ -1,17 +1,26 @@
 require_relative 'UI' 
 
-def pede_um_chute_valido chutes, erros, mascara
-    cabecalho_de_tentativa chutes, erros, mascara
-    loop do
-        chute = pede_um_chute 
-        if chutes.include? chute
-            avisa_chute_efetuado chute
-            next
-        else
-            return chute
-        end
-    end
+def save_rank nome, pontos_totais
+    conteudo = "#{nome}\n#{pontos_totais}"
+    File.write "rank.txt", conteudo
 end
+
+
+def le_rank
+    conteudo = File.read "rank.txt"
+    conteudo.split "\n"
+end
+
+
+def sorteia_palavra_secreta
+    avisa_escolhendo_palavra
+    texto = File.read ("dicionario.txt")
+    todas_as_palavras = texto.split "\n"
+    numero_escolhido = rand(todas_as_palavras.size)
+    palavra_secreta = todas_as_palavras[numero_escolhido].downcase
+    avisa_palavra_secreta_escolhida palavra_secreta
+end
+
 
 def palavra_mascarada palavra_secreta, chutes
     mascara = ""
@@ -26,12 +35,26 @@ def palavra_mascarada palavra_secreta, chutes
 end
 
 
-def joga nome
+def pede_um_chute_valido chutes, erros, mascara
+    cabecalho_de_tentativa chutes, erros, mascara
+    loop do
+        chute = pede_um_chute 
+        if chutes.include? chute
+            avisa_chute_efetuado chute
+            next
+        else
+            return chute
+        end
+    end
+end
+
+
+def joga nome, pontos_totais
     palavra_secreta = sorteia_palavra_secreta
 
     erros = 0
     chutes = []
-    pontos_ate_agora = 0
+    pontos_ate_agora = pontos_totais
     while erros < 5
         mascara = palavra_mascarada palavra_secreta, chutes
         chute = pede_um_chute_valido chutes, erros, mascara
@@ -47,8 +70,8 @@ def joga nome
                 erros += 1
             else
                 avisa_letra_encontrada total_encontrado
+                pontos_ate_agora += 10
             end    
-
         else
             if chute == palavra_secreta
                 avisa_acertou
@@ -62,14 +85,24 @@ def joga nome
         end
         avisa_pontos_ate_agora pontos_ate_agora
     end
-    
+    avisa_pontos_ate_agora pontos_ate_agora
+    pontos_ate_agora    
 end
+
 
 def jogo_da_forca
     nome = da_boas_vindas
+    pontos_totais = 0
+    avisa_campeao_atual le_rank
+
 
     loop do 
-        joga nome
+        pontos_totais += joga nome, pontos_totais
+       
+        if le_rank[1].to_i < pontos_totais
+            save_rank nome, pontos_totais
+        end
+
         break if nao_jogar_novamente?
     end
 end
